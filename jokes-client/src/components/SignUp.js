d958b85c1b9a3a1b924f55543b989ab9c70b894f
Angelo Deleon <rudy209@comcast.net>
2018-12-07 11:11:00 -0800

    Finished setting up basic jokes client

--- jokes-client/src/components/SignUp.js
+++ jokes-client/src/components/SignUp.js
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import { StyledForm } from '../styles/StyledForm';
 
-const initialUser = {
-    username: '',
-    password: ''
+
+ const initalUser = {
+  username: '',
+  password: ''
 };
 
-class SignUp extends Component {
-    state = { user: initalUser, message: '' };
 
-    handleInputChange = e => {
-        const { name, value } = e.target;
-        this.setState({ user: { ...this.state.user, [name]: value } 
-        });
-};
-
-render() {
+ class SignUp extends Component {
+  state = { user: initalUser, message: '' };
+   handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ user: { ...this.state.user, [name]: value } });
+  };
+   render() {
     const { username, password } = this.state.user;
     return (
-        <div>
-            <StyledForm >
-                <div className='form-group'>
-                <h3>Sign Up</h3>
+      <div>
+        <StyledForm>
+          <div className="form-group">
+            <h3>Sign Up</h3>
             <label htmlFor="username">Username:</label>
             <input
               type="text"
               id="username"
               value={username}
               name="username"
-              onChange={this.handleInputChange} />
+              onChange={this.handleInputChange}
+            />
           </div>
-            </StyledForm>
-        </div>
-    )
-};
-
-
-
-
-
-export default SignUp;
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <button type="submit">Sign up!</button>
+          {this.state.message ? <p>{this.state.message}</p> : null}
+        </StyledForm>
+      </div>
+    );
+  }
+}
+ export default SignUp;
